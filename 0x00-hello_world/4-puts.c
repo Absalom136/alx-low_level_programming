b39d0dfe46a690892f22aa31e96e1352c335a828
Absalom136 <absojiem@gmail.com>
2022-10-14 03:10:56 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
  *int main (void): a function to return integer
- * Return 0: program will execute successfully
+ * Return: program will execute successfully
  */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
