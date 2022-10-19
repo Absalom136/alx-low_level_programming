fa619f23ee0e0b4bf8d16920980941c9ebeb0d94
Absalom136 <absojiem@gmail.com>
2022-10-19 07:28:06 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,10 @@ int main(void)
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putchar('c')i;
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r'i);
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
