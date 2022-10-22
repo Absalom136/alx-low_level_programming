6e9b5e95095bd0a8357b9e9c94a8ca57d528c9fb
Absalom136 <absojiem@gmail.com>
2022-10-22 06:56:53 -0700

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,14 @@
 
 void print_most_numbers(void)
 {
-	int a = 0;
+	int a;
 
-	while (a < 10)
+	for (a = 0; a <= 9; a++)
 	{
 		if (a != 2 && a != 4)
+		{
 			_putchar(a + '0');
-
-		a += 1;
+		}
 	}
 	_putchar('\n');
 }
