57973dd2261a1607440dd425c0dda34e0bebb1ac
Absalom136 <absojiem@gmail.com>
2022-10-24 03:09:52 -0700

    skip 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,12 @@ void print_most_numbers(void)
 {
 	int a = 0;
 
-	for (a = 0; a <= 9; a++)
+	while (a < 10)
 	{
 		if (a != 2 && a != 4)
+			_putchar(a + '0');
 
-		_putchar(a + '0');
+		a++;
 	}
-	_putchar('\n');
+	_putchar('\n')
 }
