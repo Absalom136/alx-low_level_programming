03d6803788940ef206139f97ad68811e6d593ae2
Absalom136 <absojiem@gmail.com>
2022-10-24 12:41:01 -0700

    print numbers'

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - prints an int
+ * @n: input value to check
+ */
+
+void print_number(int n)
+{
+	unsigned int x, y;
+	int i;
+	int aux = 1;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	x = n;
+	y = x;
+	if (x > 9)
+	{
+		while (x >= 10)
+		{
+			aux = aux * 10;
+			x = x / 10;
+		}
+		_putchar((y / aux) + '0');
+		aux = aux / 10;
+
+		for (i = aux; i >= 1; i = i / 10)
+			_putchar((y / i) % 10 + '0');
+	}
+	else
+		_putchar(x + '0');
+}
